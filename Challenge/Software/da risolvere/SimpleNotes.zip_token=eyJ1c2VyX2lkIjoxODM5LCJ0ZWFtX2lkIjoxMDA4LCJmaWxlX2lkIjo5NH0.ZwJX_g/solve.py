2fe9ebe7b3e6f5e1ab22cd734a08f32ae4f27780
Greva151 <grevaval151@gmail.com>
2024-11-11 19:49:00 +0100

    16

---
+++ Challenge/Software/da risolvere/SimpleNotes.zip_token=eyJ1c2VyX2lkIjoxODM5LCJ0ZWFtX2lkIjoxMDA4LCJmaWxlX2lkIjo5NH0.ZwJX_g/solve.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# This exploit template was generated via:
+# $ pwn template SimpleNotes '--libc=./libc.so.6'
+from pwn import *
+
+# Set up pwntools for the correct architecture
+exe = context.binary = ELF(args.EXE or 'SimpleNotes')
+
+# Many built-in settings can be controlled on the command-line and show up
+# in "args".  For example, to dump all data sent/received, and disable ASLR
+# for all created processes...
+# ./exploit.py DEBUG NOASLR
+
+# Use the specified remote libc version unless explicitly told to use the
+# local system version with the `LOCAL_LIBC` argument.
+# ./exploit.py LOCAL LOCAL_LIBC
+if args.LOCAL_LIBC:
+    libc = exe.libc
+else:
+    library_path = libcdb.download_libraries('./libc.so.6')
+    if library_path:
+        exe = context.binary = ELF.patch_custom_libraries(exe.path, library_path)
+        libc = exe.libc
+    else:
+        libc = ELF('./libc.so.6')
+
+def start(argv=[], *a, **kw):
+    '''Start the exploit against the target.'''
+    if args.GDB:
+        return gdb.debug([exe.path] + argv, gdbscript=gdbscript, *a, **kw)
+    else:
+        return process([exe.path] + argv, *a, **kw)
+
+# Specify your GDB script here for debugging
+# GDB will be launched if the exploit is run via e.g.
+# ./exploit.py GDB
+gdbscript = '''
+tbreak main
+continue
+'''.format(**locals())
+
+#===========================================================
+#                    EXPLOIT GOES HERE
+#===========================================================
+# Arch:     amd64-64-little
+# RELRO:      Full RELRO
+# Stack:      Canary found
+# NX:         NX enabled
+# PIE:        PIE enabled
+# RUNPATH:    b'.'
+# Stripped:   No
+
+io = start()
+
+print(hex(exe.got['printf']))
+
+io.interactive()
+
