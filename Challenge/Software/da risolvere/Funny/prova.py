48e47d4cc93a7c64a8d0f7b59492306f18d8f930
Greva151 <grevaval151@gmail.com>
2024-11-16 00:10:54 +0100

    30

---
+++ Challenge/Software/da risolvere/Funny/prova.py
@@ -0,0 +1,11 @@
+
+
+riga1 = "brybefejnkidosboguihagsepxeywufiazxoxzbmtjrcfsknpkldhzjauxayobidvhnaqgbxzfsjyrwaanndfompthvsbqkgxmqelqaifrfkfmonzvnkmrgmhccunoir"
+riga2 = "xxxbhafmrsbuseiwnwiyibbprvienxuqromdwofcupvupdewoyjj" 
+riga3 = "ptjilemtmvowacennoaykdzojeqevimbbfmycmmgvbfzncavp" 
+riga4 = "ksdpsbjmgekyyrdzxppurwcomfjvnoouvimxjbcdbwuooxhgcjeuivfydlfucajyibhlvcsijwethshnwbmvkftvwoosexg" 
+riga5 = "rdgvipgcyliyrswfpvbrhmrgtzevpdbuggfvvlbihwbr" 
+riga6 = "pemzmblrzasbsuecggofxehwexdqowqwpbwlwqrlynupg" 
+riga7 = "ofyngrbkaldjxznvvijkpecfgdbwrkmgjinsluahzyfckykgkwlmrhrubjyfmftlafujmbijvwolwpuayyxybdolmfquldxfmvxbzliwixespnzaxjyrmrhfhozizgfvan"
+
+
